69206f2b23aac9404d96bd32e275fe32c3a57f5d
Kooshal71 <kushalganesh2@gmail.com>
2022-11-11 11:10:30 +0530

    Added Styled Components

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import styled from "styled-components";
+const ButtonStyled = styled.button`
+  padding: 2em;
+  color: black;
+  background-color: red;
+  width: 120px;
+`;
 export default function Button(props) {
   return (
     <div>
       <Link to={props.path}>
-        <button type="button">{props.title}</button>
+        <ButtonStyled type="button">{props.title}</ButtonStyled>
       </Link>
     </div>
   );
--- src/Components/Keypad.jsx
+++ src/Components/Keypad.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 import Button from "./Button";
+
 export default function Keypad() {
   return (
     <>
