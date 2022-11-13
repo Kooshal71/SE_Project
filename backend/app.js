b6bac9efcb52d6c8c02944185b59a056c8f8f55c
Kooshal71 <kushalganesh2@gmail.com>
2022-11-13 18:06:26 +0530

    Withdraw Function Backend Complete

--- backend/app.js
+++ backend/app.js
@@ -108,7 +108,67 @@ app.put("/beers", (req, res) => {
 });
 */
 
-app.get("/withdraw", (req, res) => {});
+app.put("/withdraw", async function (req, res) {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected to Database with id ${connection.threadId}`);
+    const { pin, cNum, amount } = req.body;
+    let client_id = "";
+    let finalBalance = 0;
+    connection.query(
+      `SELECT client_id FROM client WHERE card_no = ?`,
+      [cNum],
+      (err, rows) => {
+        //connection.release();
+        if (err) throw err;
+        console.log(rows);
+        client_id = rows[0].client_id;
+        console.log(`Client ID is ${client_id}`);
+        console.log(`Client ID coming out is ${client_id}`);
+        connection.query(
+          "SELECT balance FROM balance_inquiries WHERE client_id = ?",
+          [client_id],
+          (err, rows) => {
+            //connection.release();
+            if (err) throw err;
+            console.log(rows);
+            let balance = rows[0].balance;
+            console.log(`Balance is ${balance}`);
+            if (balance - amount > 0) finalBalance = balance - amount;
+            connection.query(
+              "UPDATE balance_inquiries SET balance = ? WHERE client_id = ?",
+              [finalBalance, client_id],
+              (err, rows) => {
+                connection.release();
+                if (err) throw err;
+                console.log(
+                  `Client with ${client_id} has a final balance of ${finalBalance}`
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+    res.send("Completed");
+    console.log(req.body);
+  });
+});
+
+app.get("/fetch", (req, res) => {
+  pool.getConnection((error, connection) => {
+    if (error) throw error;
+    console.log("Connected to the database");
+
+    //? query
+    connection.query("SELECT * FROM balance_inquiries", (err, rows) => {
+      connection.release();
+      if (err) throw err;
+      res.send(rows);
+    });
+  });
+});
+
 app.listen(port, () => console.log("listening on port 5000"));
 
 app.get("/", (req, res) => {
